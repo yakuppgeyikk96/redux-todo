a7c3cc8210374a42cb92cb33420e8367b659f53d
Yakup Geyik <yakuppgeyik@gmail.com>
2022-07-03 23:10:00 +0300

    redux todo

---
+++ src/redux/todos/todosSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const todosSlice = createSlice({
+  name: 'todos',
+  initialState: {
+    items: [],
+    activeFilter: 'all',
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.items.push(action.payload)
+    },
+    toggle: (state, action) => {
+      const { id } = action.payload
+      const item = state.items.find((item) => item.id === id)
+      item.completed = !item.completed
+    },
+    destroy: (state, action) => {
+      const id = action.payload
+      const filtered = state.items.filter((item) => item.id !== id)
+      state.items = filtered
+    },
+    changeActiveFilter: (state, action) => {
+      state.activeFilter = action.payload
+    },
+    clearCompleted: (state) => {
+      const filter = state.items.filter((item) => item.completed === false)
+      state.items = filter
+    },
+  },
+})
+
+export const { addTodo, toggle, destroy, changeActiveFilter, clearCompleted } =
+  todosSlice.actions
+
+export default todosSlice.reducer
